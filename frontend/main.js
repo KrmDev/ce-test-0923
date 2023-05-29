2c9415b1701d780a8f4de546249e91c0d9931f5f
Karim <Bentahar.dev@gmail.com>
2023-05-29 18:38:20 +0200

    Ajout des requêtes API

--- frontend/main.js
+++ frontend/main.js
@@ -7,130 +7,155 @@ const portal = document.querySelector(".portal");
 let state = 0;
 
 function animateStar(star, isReverse = false) {
-    star.classList.add('animate');
+  star.classList.add('animate');
 
-    setTimeout(function() {
-        star.classList.add(isReverse ? 'star-rounded' : 'star-pointy');
-    }, 1000);
+  setTimeout(function() {
+    star.classList.add(isReverse ? 'star-rounded' : 'star-pointy');
+  }, 1000);
 
-    setTimeout(function() {
-        star.classList.remove('animate', isReverse ? 'star-pointy' : 'star-rounded');
-    }, 2000);
+  setTimeout(function() {
+    star.classList.remove('animate', isReverse ? 'star-pointy' : 'star-rounded');
+  }, 2000);
 }
 
-
 greyStar.addEventListener('click', function() {
-    animateStar(greyStar);
+  animateStar(greyStar);
 
-    setTimeout(function() {
-        greyStar.classList.add('hidden');
-        greyStarRounded.classList.remove('hidden');
-        animateStar(greyStarRounded);
-    }, 2000);
+  setTimeout(function() {
+    greyStar.classList.add('hidden');
+    greyStarRounded.classList.remove('hidden');
+    animateStar(greyStarRounded);
+  }, 2000);
 
-    setTimeout(function() {
-        portal.classList.add('open');
-    }, 4000);
+  setTimeout(function() {
+    portal.classList.add('open');
+    sendStarData("add", 1, 42); 
+  }, 4000);
 
-    setTimeout(function() {
-        greyStarRounded.classList.add('hidden');
-        yellowStarRounded.classList.remove('hidden');
-        animateStar(yellowStarRounded, true);
-    }, 6000);
+  setTimeout(function() {
+    greyStarRounded.classList.add('hidden');
+    yellowStarRounded.classList.remove('hidden');
+    animateStar(yellowStarRounded, true);
+  }, 6000);
 
-    setTimeout(function() {
-        yellowStarRounded.classList.add('hidden');
-        yellowStar.classList.remove('hidden');
-        animateStar(yellowStar);
-    }, 7000);
+  setTimeout(function() {
+    yellowStarRounded.classList.add('hidden');
+    yellowStar.classList.remove('hidden');
+    animateStar(yellowStar);
+  }, 7000);
 
-    setTimeout(function() {
-        portal.classList.remove('open');
-        state = 1;
-    }, 8000);
+  setTimeout(function() {
+    portal.classList.remove('open');
+    state = 1;
+  }, 8000);
 });
 
 yellowStar.addEventListener('click', function() {
-    animateStar(yellowStar, portal, true);
+  animateStar(yellowStar, true);
+  sendStarData("remove", 1, 42); 
 });
 
 function handleClick() {
-    if (state === 0) {
-       
-        portal.classList.remove("hidden");
-        portal.classList.add("opening");
-        portal.style.zIndex = "3"; 
+  if (state === 0) {
+    portal.classList.remove("hidden");
+    portal.classList.add("opening");
+    portal.style.zIndex = "3";
+    greyStar.classList.add("animate");
 
-        
-        greyStar.classList.add("animate");
+    setTimeout(() => {
+      greyStar.classList.add("hidden");
+      greyStarRounded.classList.remove("hidden");
+      greyStarRounded.classList.add("animate");
+    }, 2000);
 
-        setTimeout(() => {
-            greyStar.classList.add("hidden");
-            greyStarRounded.classList.remove("hidden");
-            greyStarRounded.classList.add("animate");
-        }, 2000);
+    setTimeout(() => {
+      greyStarRounded.classList.add("hidden");
+      yellowStar.classList.remove("hidden");
+      yellowStar.classList.add("animate");
+      portal.style.zIndex = "1";
+    }, 4000);
 
-        setTimeout(() => {
-            greyStarRounded.classList.add("hidden");
-            yellowStar.classList.remove("hidden");
-            yellowStar.classList.add("animate");
-            portal.style.zIndex = "1";
-        }, 4000);
+    setTimeout(() => {
+      yellowStar.classList.add("hidden");
+      yellowStarRounded.classList.remove("hidden");
+      portal.classList.remove("opening");
+      portal.classList.add("closing");
+    }, 6000);
 
-        setTimeout(() => {
-            yellowStar.classList.add("hidden");
-            yellowStarRounded.classList.remove("hidden");
-            portal.classList.remove("opening");
-            portal.classList.add("closing");
-        }, 6000);
+    setTimeout(() => {
+      portal.classList.add("hidden");
+      portal.classList.remove("closing");
+      state = 1;
+    }, 8000);
 
-        setTimeout(() => {
-            portal.classList.add("hidden");
-            portal.classList.remove("closing");
-            state = 1;
-        }, 8000);
+    sendStarData("add", 1, 42); 
+  } else {
+    portal.classList.remove("hidden");
+    portal.classList.add("opening");
+    portal.style.zIndex = "3";
+    yellowStarRounded.classList.add("animateReverse");
 
-    } else {
+    setTimeout(() => {
+      yellowStarRounded.classList.add("hidden");
+      yellowStar.classList.remove("hidden");
+      yellowStar.classList.add("animateReverse");
+    }, 2000);
 
-        portal.classList.remove("hidden");
-        portal.classList.add("opening");
-        portal.style.zIndex = "3"; 
+    setTimeout(() => {
+      yellowStar.classList.add("hidden");
+      greyStarRounded.classList.remove("hidden");
+      greyStarRounded.classList.add("animateReverse");
+      portal.style.zIndex = "1";
+    }, 4000);
 
-        
-        yellowStarRounded.classList.add("animateReverse");
+    setTimeout(() => {
+      greyStarRounded.classList.add("hidden");
+      greyStar.classList.remove("hidden");
+      portal.classList.remove("opening");
+      portal.classList.add("closing");
+    }, 6000);
 
-        setTimeout(() => {
-            yellowStarRounded.classList.add("hidden");
-            yellowStar.classList.remove("hidden");
-            yellowStar.classList.add("animateReverse");
-        }, 2000);
+    setTimeout(() => {
+      portal.classList.add("hidden");
+      portal.classList.remove("closing");
+      state = 0;
+    }, 8000);
 
-        setTimeout(() => {
-            yellowStar.classList.add("hidden");
-            greyStarRounded.classList.remove("hidden");
-            greyStarRounded.classList.add("animateReverse");
-            portal.style.zIndex = "1"; 
-        }, 4000);
-
-        setTimeout(() => {
-            greyStarRounded.classList.add("hidden");
-            greyStar.classList.remove("hidden");
-            portal.classList.remove("opening");
-            portal.classList.add("closing");
-        }, 6000);
-
-        setTimeout(() => {
-            portal.classList.add("hidden");
-            portal.classList.remove("closing");
-            state = 0;
-        }, 8000);
-    }
+    sendStarData("remove", 1, 42); 
+  }
 }
 
 greyStar.addEventListener("click", handleClick);
 greyStarRounded.addEventListener("click", handleClick);
 yellowStar.addEventListener("click", handleClick);
 yellowStarRounded.addEventListener("click", handleClick);
+
+function sendStarData(endpoint, mentorId, studentId) {
+  const url = `http://localhost/star/${endpoint}`;
+  const data = JSON.stringify({ idMentor: mentorId, idStudent: studentId });
+
+  fetch(url, {
+    method: endpoint === "add" ? "POST" : "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: data
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Une erreur est survenue lors de la requête API.");
+      }
+      return response.json();
+    })
+    .then(result => {
+      console.log("Réponse de l'API :", result);
+    })
+    .catch(error => {
+      console.error("Erreur :", error);
+    });
+}
+
+
 
 
 
